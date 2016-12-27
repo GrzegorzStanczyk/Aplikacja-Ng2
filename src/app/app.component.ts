ef21a3f6c9fcf145382755bbacc996ca0e4990d8
Grzegorz Stańczyk <grzegorz.stanczyk@o2.pl>
2016-12-27 01:41:58 +0100

    L11 Użycie dyrektywy ngStyle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,15 +12,24 @@ export class AppComponent {
     name: 'The best of Eduweb',
     tracks: 23,
     color: '#FF0000',
-    favourite: true
+    favourite: true,
+    size: 130
   }
 
   selected = null;
 
-  size = 120;
+  
 
   save(event) {
     console.log('Zapisano', event);
+  }
+
+  getPlaylistStyle(playlist) {
+    return {
+      borderBottomColor: playlist.color, 
+      color: playlist.color,
+      fontSize: playlist.size+"%"
+    }
   }
 
 // Funkcja wykonywana w momencie tworzenia klasy
