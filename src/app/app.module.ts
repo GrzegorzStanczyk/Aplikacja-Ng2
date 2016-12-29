f9453b0ae829183e68d5190b888587b419c71514
Grzegorz Stańczyk <grzegorz.stanczyk@o2.pl>
2016-12-30 00:38:32 +0100

    L17A Tworzenie komponentu za pomocą konsoli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { PlaylistsComponent } from './playlists/playlists.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlaylistsComponent
   ],
   imports: [
     BrowserModule,
