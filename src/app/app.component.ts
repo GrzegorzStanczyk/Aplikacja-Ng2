e875aa387570532d15ee2beb2e9546080392adf1
Grzegorz Stańczyk <grzegorz.stanczyk@o2.pl>
2016-12-30 00:44:48 +0100

    L17 Dzielenie aplikacji na komponenty

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,59 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'witaj w kursie Angular!';
-
-  selected = null;
-
-  edited = {
-
-  }
-
-  mode = "none";
-
-  playlists = [
-    {
-      name: 'The best of Eduweb',
-      tracks: 23,
-      color: '#0000FF',
-      favourite: false
-    },
-    {
-      name: 'Angular Greatest Hits',
-      tracks: 2,
-      color: '#FF0000',
-      favourite: true
-    }
-  ]
-
-  select(playlist) {
-    // zapobiegnięcie znikania okna playlisty podczas klikania albumu na liście. 
-    if(playlist !== this.selected)
-    this.mode = "selected";
-    this.selected = playlist;
-  }
-
-  edit(playlist) {
-    this.mode = "edit";
-    this.edited = playlist;
-    this.selected = playlist;
-  }
   
-  createNew() {
-    this.mode = "edit";
-    var newPlaylist = {};
-    this.selected = newPlaylist;
-    this.edited = newPlaylist;
-  }
-
-  getPlaylistStyle(playlist) {
-    return {
-      borderBottomColor: playlist.color
-    }
-  }
-
-  save(event) {
-    console.log("Zapisano", event)
-  }
 // Funkcja wykonywana w momencie tworzenia klasy
   constructor() {
     
---
+++ src/app/playlists/playlists.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+
+// Dekoraotr
+@Component({
+  selector: 'playlists',
+  templateUrl: './playlists.component.html',
+  styleUrls: ['./playlists.component.css']
+})
+export class PlaylistsComponent implements OnInit {
+
+  selected = null;
+
+  edited = {
+
+  }
+
+  mode = "none";
+
+  playlists = [
+    {
+      name: 'The best of Eduweb',
+      tracks: 23,
+      color: '#0000FF',
+      favourite: false
+    },
+    {
+      name: 'Angular Greatest Hits',
+      tracks: 2,
+      color: '#FF0000',
+      favourite: true
+    }
+  ]
+
+  select(playlist) {
+    // zapobiegnięcie znikania okna playlisty podczas klikania albumu na liście. 
+    if(playlist !== this.selected)
+    this.mode = "selected";
+    this.selected = playlist;
+  }
+
+  edit(playlist) {
+    this.mode = "edit";
+    this.edited = playlist;
+    this.selected = playlist;
+  }
+  
+  createNew() {
+    this.mode = "edit";
+    var newPlaylist = {};
+    this.selected = newPlaylist;
+    this.edited = newPlaylist;
+  }
+
+  getPlaylistStyle(playlist) {
+    return {
+      borderBottomColor: playlist.color
+    }
+  }
+
+  save(event) {
+    console.log("Zapisano", event)
+  }
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
