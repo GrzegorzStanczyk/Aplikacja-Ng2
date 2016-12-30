012471948eedc396a0f17d2a1ebd0aeead657754
Grzegorz Stańczyk <grzegorz.stanczyk@o2.pl>
2016-12-31 00:01:35 +0100

    L21C Projekcja z ngContent.

--- src/app/playlists/playlist-form.component.ts
+++ src/app/playlists/playlist-form.component.ts
@@ -3,20 +3,10 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'playlist-form',
   template: `
-    <div class="card">
-      <div class="card-block">
-        <h4 class="card-title">Playlista</h4>
-        <p class="card-text">Opis playlisty.</p>
-      </div>
-      <!--* odpina element od drzewa DOM i zamienia na szablon-->
-      <div class="card-block">
+      <div>
         <div class="form-group">
           <label for="">Name:</label>
-          <!--Sposób na odniesienie się do właściwości i jej wyświtlenie przez [] nawaias kwadratowy-->
-          <!--Na zdarzenie (keyup) wykonuję kod "playlist.name = $event.target.value", który pobiera wartość lub stan elementu i aktualizuje dane-->
           <input type="text" [(ngModel)]="playlist.name" class="form-control">
-                                  <!--[xxx]="zmienna" (xxxChange)="zmienna = $event"
-                                      [(xxx)] = "zmienna"-->
         </div>
         <div class="form-group">
           <label for="">Tracks:</label>
@@ -32,10 +22,8 @@ import { Component, OnInit, Input } from '@angular/core';
         </div>
         <div class="form-group">
           <button class="btn btn-success float-xs-right" (click)="save($event)">Zapisz</button>
-          <!--Jeśli chce pobierać zdarzenie które się dzieje na komponencie to używam () np. (click)="save($event), a jeśli przekazuje dane do komponentu to [] np. [checked]="playlist.favourites",-->
         </div>
-      </div><!--card-block-->
-    </div><!--edited-->
+      </div>
   `,
   styles: []
 })
