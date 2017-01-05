a9258e01aa4427206defc4a38c04a810b695010e
Grzegorz Stańczyk <grzegorz.stanczyk@o2.pl>
2017-01-05 20:14:15 +0100

    L35 Navigacja z History API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { PlaylistsModule } from './playlists/playlists.module';
 import { MusicSearchModule } from './music-search/music-search.module';
 import { routerModule } from './app.routing';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
