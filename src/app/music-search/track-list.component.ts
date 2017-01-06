f4d63b96856f773b0c93a4b28720b1173d8f74a6
Grzegorz Stańczyk <grzegorz.stanczyk@o2.pl>
2017-01-07 00:14:32 +0100

    L40 Element <audio> do odtwarzania utworów

--- src/app/music-search/track-list.component.ts
+++ src/app/music-search/track-list.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'track-list',
   template: `
+  <video [src]="currentSrc" controls style="width:100%"></video>
   <table class="table table-striped">
       <thead>
         <tr>
@@ -12,7 +13,7 @@ import { Component, OnInit, Input } from '@angular/core';
         </tr>
       </thead>
       <tbody>
-        <tr *ngFor="let track of tracks">
+        <tr *ngFor="let track of tracks" (click)="play(track)">
             <td> {{track.track_number}} </td>
             <td> {{track.name}} </td>
             <td> {{track.artists[0].name}} </td>
@@ -23,6 +24,12 @@ import { Component, OnInit, Input } from '@angular/core';
   styles: []
 })
 export class TrackListComponent implements OnInit {
+
+  currentSrc = "";
+
+  play(track) {
+    this.currentSrc = track.preview_url;
+  }
 
   @Input()
   tracks
